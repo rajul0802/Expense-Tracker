3775ecf33b0292510ed41bdaac97337377bb2d69
rajul0802 <9454344849myno@gmail.com>
2025-08-19 23:15:14 +0530

    helo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Expense-tracker/', // set to your repo/subfolder name
+  base: './', // set to your repo/subfolder name
 })
 
